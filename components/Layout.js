adab13b5a7a4a296767d56947be5f5f9b08bbc24
Lee Robinson <lrobinson2011@gmail.com>
2020-10-03 12:16:18 -0500

    Update default theme.

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { useState } from 'react';
 
 export default function Layout({ children, meta: pageMeta }) {
-  const [theme, setTheme] = useState('tomorrow');
+  const [theme, setTheme] = useState('okaidia');
   const meta = {
     title: 'Prism with Next.js',
     description:
@@ -76,9 +76,9 @@ export default function Layout({ children, meta: pageMeta }) {
                   value={theme}
                   className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 >
+                  <option value="okaidia">Okaidia</option>
                   <option value="tomorrow">Tomorrow</option>
                   <option value="coy">Coy</option>
-                  <option value="okaidia">Okaidia</option>
                   <option value="funky">Funky</option>
                 </select>
                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
